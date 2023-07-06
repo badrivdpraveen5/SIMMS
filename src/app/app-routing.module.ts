a9a9b714b3c22fccb2bb978dc65df8222d0ca2ca
Badri Venkata Durga Praveen <badrivdpraveen5@gmail.com>
2023-07-06 23:25:13 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,20 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AdminloginComponent } from './homepage/adminlogin/adminlogin.component';
 import { FacultyloginComponent } from './homepage/facultylogin/facultylogin.component';
 import { StudentloginComponent } from './homepage/studentlogin/studentlogin.component';
+import { HomeComponent } from './homepage/home/home.component';
+import { AboutusComponent } from './homepage/aboutus/aboutus.component';
+import { ContactusComponent } from './homepage/contactus/contactus.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'',redirectTo:'home/index',pathMatch:'full'},
   {path:'home',component:HomepageComponent,
   children:[
+    {path:'index',component:HomeComponent},
     {path:'adminlogin',component:AdminloginComponent},
     {path:'facultylogin',component:FacultyloginComponent},
-    {path:'studentlogin',component:StudentloginComponent}
+    {path:'studentlogin',component:StudentloginComponent},
+    {path:'aboutus',component:AboutusComponent},
+    {path:'contactus',component:ContactusComponent}
   ]
 }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AdminloginComponent } from './homepage/adminlogin/adminlogin.component';
 import { FacultyloginComponent } from './homepage/facultylogin/facultylogin.component';
 import { StudentloginComponent } from './homepage/studentlogin/studentlogin.component';
+import { HomeComponent } from './homepage/home/home.component';
+import { AboutusComponent } from './homepage/aboutus/aboutus.component';
+import { ContactusComponent } from './homepage/contactus/contactus.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { StudentloginComponent } from './homepage/studentlogin/studentlogin.comp
     HomepageComponent,
     AdminloginComponent,
     FacultyloginComponent,
-    StudentloginComponent
+    StudentloginComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
