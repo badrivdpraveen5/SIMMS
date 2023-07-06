564ca1d12d2f34fa2848eb8fedc165895b2235b4
Badri Venkata Durga Praveen <badrivdpraveen5@gmail.com>
2023-07-06 20:50:22 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AdminloginComponent } from './homepage/adminlogin/adminlogin.component';
+import { FacultyloginComponent } from './homepage/facultylogin/facultylogin.component';
+import { StudentloginComponent } from './homepage/studentlogin/studentlogin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomepageComponent,
+  children:[
+    {path:'adminlogin',component:AdminloginComponent},
+    {path:'facultylogin',component:FacultyloginComponent},
+    {path:'studentlogin',component:StudentloginComponent}
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AdminloginComponent } from './homepage/adminlogin/adminlogin.component';
+import { FacultyloginComponent } from './homepage/facultylogin/facultylogin.component';
+import { StudentloginComponent } from './homepage/studentlogin/studentlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AdminloginComponent,
+    FacultyloginComponent,
+    StudentloginComponent
   ],
   imports: [
     BrowserModule,
